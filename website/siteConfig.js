51b2e148917caeab42912f89cc65c90c2f2608d1
endiliey <endiliey@gmail.com>
2018-05-17 15:50:07 +0800

    minor edit

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -63,6 +63,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {blog: true, label: 'Blog'},
+    {href: "https://github.com/endiliey", label: "GitHub" }
   ],
 
   // If you have users set above, you add it here:
