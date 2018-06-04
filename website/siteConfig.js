f124c4435e1d0a348fe56e44138e34e7b86905bd
endiliey <endiliey@gmail.com>
2018-06-04 18:23:30 +0800

    better site config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -45,82 +45,62 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Endilie Yacop Sucipto' /* title for your website */,
+  title: 'Endilie Yacop Sucipto',
   tagline: 'I love building things' ,
-  url: 'https://endiliey.github.io' /* your website url */,
-  baseUrl: '/' /* base url for your project */,
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
-
-  // Used for publishing and more
+  url: 'https://endiliey.github.io',
+  baseUrl: '/',
   cname: 'endiliey.com',
+  noIndex: false,
   projectName: 'endiliey.github.io',
   organizationName: 'endiliey',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
-
-  // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'docusaurus/introduction', label: 'Docs'},
     {blog: true, label: 'Blog'},
     {href: "https://forum.endiliey.com", label: "Forum" },
     {href: "https://github.com/endiliey", label: "GitHub" }
   ],
-
-  // If you have users set above, you add it here:
   users,
-
-  /* path to images for header/footer */
   headerIcon: 'img/elogo.gif',
   footerIcon: 'img/elogo.gif',
   favicon: 'img/elogo.gif',
-
-  /* colors for website */
+  /*
+  algolia: {
+    apiKey: '',
+    indexName: 'endiliey',
+  },
+  */
   colors: {
     primaryColor: '#28ACE2',
     secondaryColor: '#1E81AA',
   },
-
-  /* custom fonts for website */
-  /*fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },*/
-
-  // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
+  translationRecruitingLink: 'https://crowdin.com/project/endiliey',
   copyright:
     'Copyright © ' +
     new Date().getFullYear() +
     ' Endilie Yacop Sucipto',
 
   highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks
-    theme: 'default',
+    theme: 'atom-one-dark',
   },
 
   // Add custom scripts here that would be placed in <script> tags
-  scripts: ['https://buttons.github.io/buttons.js'],
+  scripts: [
+    'https://buttons.github.io/buttons.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
+    '/js/code-blocks-buttons.js',
+  ],
 
-  stylesheets: ['https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'],
+  stylesheets: [
+    'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    '/css/code-blocks-buttons.css',
+  ],
 
   /* On page navigation for the current documentation page */
   onPageNav: 'separate',
 
   /* Open Graph and Twitter card images */
-  ogImage: 'img/docusaurus.png',
-  twitterImage: 'img/docusaurus.png',
-
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+  ogImage: 'img/elogo.gif',
+  twitterImage: 'img/elogo.gif',
 };
 
 module.exports = siteConfig;
